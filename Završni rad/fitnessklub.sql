3974aefb4aeb32dd5d0ce6e2d195d1bbba9493b3
Dražen Kalazić <thekalazic@gmail.com>
2024-05-21 17:15:29 +0200

    promjena na bazi podataka

--- Završni rad/fitnessklub.sql
+++ Završni rad/fitnessklub.sql
@@ -13,14 +13,15 @@ ime varchar(25) not null,
 prezime varchar(50) not null,
 email varchar(155) not null,
 userid varchar(25) not null,
-pwd varchar(25) not null
+pass varchar(25) not null
 );
 
 create table privatni_treneri(
 id int not null primary key identity(10,1),
 ime varchar(25) not null,
 prezime varchar(50) not null,
-email varchar(155) not null
+email varchar(155) not null,
+cijena_sat decimal(3,2)
 );
 
 create table clanovi(
@@ -29,7 +30,7 @@ ime varchar(25) not null,
 prezime varchar(50) not null,
 email varchar(155) not null,
 grupa int not null,
-verificiran bit not null
+clan_od datetime not null
 );
 
 create table grupe(
@@ -37,9 +38,9 @@ id int not null primary key identity(10,1),
 naziv varchar(155) not null,
 clan int not null,
 privatni_trener int not null,
-maxclanova int not null,
+kolicina_clanova int not null,
 cijena decimal(10,2)
 );
 
-alter table grupe add foreign key (id) references clanovi(id);
+alter table clanovi add foreign key (id) references grupe(id);
 alter table grupe add foreign key (id) references privatni_treneri(id);
