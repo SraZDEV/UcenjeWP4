583d0b8834a3ff13ce529b1f1c2d9c5f94e7f6cd
Dražen Kalazić <thekalazic@gmail.com>
2024-06-04 18:07:51 +0200

    igrali smo se s ispisom

--- CSHARP/Ucenje/UcenjeCS/Program.cs
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*
+ * 
+ * Komentar
+ * kroz više redova
+ * 
+ */
 Console.WriteLine("Hello, World!");
+Console.WriteLine(7);
+Console.WriteLine(true);
+
+Console.WriteLine("Dražen Kalazić");
+
+
