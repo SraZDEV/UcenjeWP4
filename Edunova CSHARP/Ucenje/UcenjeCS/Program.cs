7b1ea911895378dea664e011f4a30dbe9c728fde
Dražen Kalazić <thekalazic@gmail.com>
2024-07-11 17:00:58 +0200

    .

---
+++ Edunova CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -0,0 +1,94 @@
+﻿
+
+
+
+using UcenjeCS;
+using UcenjeCS.E13KlasaObjekt;
+
+new UcenjeCS.E18KonzolnaAplikacija.Izbornik();
+
+//new UcenjeCS.E17Subota.Raspored();
+
+//new UcenjeCS.E17Subota.Program();
+
+//new UcenjeCS.E16GenericiLambdaEkstenzije.Program();
+
+//new UcenjeCS.E15RadSStringovima.Program();
+
+//new UcenjeCS.E14Nasljedivanje.Program();
+
+//UcenjeCS.E13KlasaObjekt.Program.Izvedi();
+
+
+//E12Rekurzija.Izvedi();
+
+//E11Z1.Izvedi();
+
+//E11Metode.Izvedi();
+
+//E10Z2.Izvedi();
+
+//E10TryCatch.Izvedi();
+
+//E09ForEachPetlja.Izvedi();
+
+//E08DoWhilePetlja.Izvedi();
+
+//E07WhilePetlja.Izvedi();
+
+// mjerenje vremena izvođenja
+//var p = DateTime.Now;
+//E06ForPetlja.Izvedi();
+//Console.WriteLine(DateTime.Now-p);
+
+//E05Z3.Izvedi();
+
+//E05Z2.Izvedi();
+
+//E05Z1.Izvedi();
+
+
+
+//E03Z4.Izvedi();
+//E03Z3.Izvedi();
+
+//E01Z9.Izvedi();
+//E01Z8.Izvedi();
+//E01Z7.Izvedi();
+
+
+//E01Z6.Izvedi();
+//E01Z5.Izvedi();
+//E01Z4.Izvedi();
+
+//E05Nizovi.Izvedi();
+
+//E04UvjetnoGrananjeSwitch.Izvedi();
+//E03Z1.Izvedi();
+//E03UvjetnoGrananjeIf.Izvedi();
+//E01Z2.Izvedi();
+//E01Z1.Izvedi();
+//E02VarijableTipoviPodatakaOperatori.Izvedi();
+//E01UlazIzlaz.Izvedi();
+
+
+
+
+
+// See https://aka.ms/new-console-template for more information
+/*
+ * 
+ * Komentar kroz
+ * više redova
+ * 
+ */
+/*
+Console.WriteLine("Hello, World!");
+Console.WriteLine(7);
+Console.WriteLine(true);
+
+Console.WriteLine("Tomislav");
+
+*/
+
+
